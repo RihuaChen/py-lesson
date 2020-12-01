5924617d766d156801241b3ab334b3dad8f422c5
ricky chen <rickychenrihua@foxmail.com>
2020-12-01 16:50:40 +0800

    commit -1
    commit -2
    
    commit -3
    
    commit -4
    
    commit -5

--- print.py
+++ print.py
@@ -6,6 +6,8 @@
 #         print('{}x{}={}\t'.format(i, j, i*j), end='')
 #     print()
 
+
+a = 'b'
 year = int(input('请输入年份：  '))
 if year % 4 == 0 and year % 100 != 0 or year % 400 == 0:
     print('{}是闰年'.format(year))
--- test.py
+++ test.py
@@ -1,3 +1,8 @@
 print('hello world')
 print('hello world')
 print('hello world3')
+print('hello world4')
+print('hello world4')
+print('hello world5')
+print('hello world5')
+print('hello world6')
